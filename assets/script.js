16338d976d16de1603905aaa9d8eab128ee24181
Thiago Vieira <tgvp.prog@gmail.com>
2022-04-15 19:21:18 -0300

    upgrade

--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     $('.droid').on('click', function(){
         $(".head").toggleClass("move-droid-head");
         $(".radio,.shadow").toggleClass("move-droid");
-        $(".body").toggleClass("move-droid-rotate");
-              
+        $(".body").toggleClass("move-droid-rotate");        
     });
 });
